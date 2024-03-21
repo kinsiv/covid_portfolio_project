1758e5b2e9ca2fe11b6a270e7b8442b8710bb153
Hayden A <hayden.all.i@icloud.com>
2024-03-21 12:25:47 -0400

    Update View Setup Clauses.sql

--- SQL/View Setup Clauses.sql
+++ SQL/View Setup Clauses.sql
@@ -1,19 +1,26 @@
 -- FIRST SECTION: Vaccination view statements
 
 -- Removing the NULL values that persisted after a Power Query edit.
+BEGIN TRANSACTION;
 SELECT * FROM vaccinations WHERE tests_units IS NOT NULL;
 UPDATE dbo.vaccinations SET tests_units=0 WHERE tests_units IS NULL;
+COMMIT TRANSACTION;
 
+BEGIN TRANSACTION;
 SELECT * FROM vaccinations WHERE continent IS NULL ORDER BY location;
 UPDATE vaccinations SET continent='Africa' WHERE location='Africa' AND continent IS NULL;
 UPDATE vaccinations SET continent='Asia' WHERE location='Asia' OR location='Oceania';
 UPDATE vaccinations SET continent='Europe' WHERE location LIKE '%Europe%';
 UPDATE vaccinations SET continent='North America' WHERE location='North America';
 UPDATE vaccinations SET continent='South America' WHERE location='South America';
+COMMIT TRANSACTION;
 
+BEGIN TRANSACTION;
 SELECT * FROM vaccinations WHERE location LIKE '%income' OR location='World';
 UPDATE vaccinations SET continent='N/A' WHERE location LIKE '%income' OR location='World';
+COMMIT TRANSACTION;
 
+BEGIN TRANSACTION;
 -- Removing timestamp from date attribute.
 ALTER TABLE vaccinations ALTER COLUMN date DATE;
 
@@ -24,22 +31,27 @@ UPDATE vaccinations SET month_digit=MONTH(date);
 -- Creating a year column for queries.
 ALTER TABLE vaccinations ADD year SMALLINT;
 UPDATE vaccinations SET year=YEAR(date);
-
+COMMIT TRANSACTION;
 
 
 -- SECOND SECTION: Deaths view statements
 
 -- Removing the NULL values that persisted after a Power Query edit.
+BEGIN TRANSACTION;
 SELECT * FROM deaths WHERE continent IS NULL ORDER BY location;
 UPDATE deaths SET continent='Africa' WHERE location='Africa' AND continent IS NULL;
 UPDATE deaths SET continent='Asia' WHERE location='Asia' OR location='Oceania';
 UPDATE deaths SET continent='Europe' WHERE location LIKE '%Europe%';
 UPDATE deaths SET continent='North America' WHERE location='North America';
 UPDATE deaths SET continent='South America' WHERE location='South America';
+COMMIT TRANSACTION;
 
+BEGIN TRANSACTION;
 SELECT * FROM deaths WHERE location LIKE '%income' OR location='World';
 UPDATE deaths SET continent='N/A' WHERE location LIKE '%income' OR location='World';
+COMMIT TRANSACTION;
 
+BEGIN TRANSACTION;
 -- Removing timestamp from date attribute.
 ALTER TABLE deaths ALTER COLUMN date DATE;
 
@@ -51,3 +63,4 @@ UPDATE deaths SET month_digit=MONTH(date);
 ALTER TABLE deaths ADD year TINYINT;
 ALTER TABLE deaths ALTER COLUMN year SMALLINT;
 UPDATE deaths SET year=YEAR(date);
+COMMIT TRANSACTION;
